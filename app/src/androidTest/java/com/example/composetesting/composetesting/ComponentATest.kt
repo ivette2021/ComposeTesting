9fd10a93f77083e409eb621c049fd59b66d91edd
Ivette C Valdivia Gomez <89611005+ivette2021@users.noreply.github.com>
2023-11-09 16:26:19 -0300

    finders

--- app/src/androidTest/java/com/example/composetesting/composetesting/ComponentATest.kt
+++ app/src/androidTest/java/com/example/composetesting/composetesting/ComponentATest.kt
@@ -2,6 +2,11 @@ package com.example.composetesting.composetesting
 
 
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onAllNodesWithContentDescription
+import androidx.compose.ui.test.onAllNodesWithTag
+import androidx.compose.ui.test.onAllNodesWithText
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
 import com.example.composetesting.components.ComponentA
 
 import org.junit.Rule
@@ -16,5 +21,19 @@ class ComponentATest {
         composeTestRule.setContent {
             ComponentA()
         }
+
+
+        //Finders
+        composeTestRule.onNodeWithText(
+            "ivette",
+            ignoreCase = true
+        )//como este parametroe sta en minuscula agregamos ingnorar caso
+        composeTestRule.onAllNodesWithTag("component 2")
+        composeTestRule.onNodeWithContentDescription("sImage")//traemos un una imagen induvudual por su id
+
+        composeTestRule.onAllNodesWithTag("xsImage") //con esta linea traemos todos las imagenes con el mismo id
+        //  composeTestRule.onAllNodesWithText(":") //busca todos los textos que tengan dos puntos
+        composeTestRule.onAllNodesWithText("i") //busca todos los textos que tengan i puntos
+        composeTestRule.onAllNodesWithContentDescription("decoration") //te busca todos los que tengan como descripcion decoracion
     }
 }
--- app/src/main/java/com/example/composetesting/components/ComponentA.kt
+++ app/src/main/java/com/example/composetesting/components/ComponentA.kt
@@ -1,9 +1,27 @@
 package com.example.composetesting.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 
 
 @Composable
 fun ComponentA() {
-//TODO
+    Column(Modifier.fillMaxSize()){
+        Text(text = "Ivette",Modifier.testTag("component1"))
+        Text(text = "Ivi", Modifier.testTag("component2"))
+        Text(text = "Ihiviko", Modifier.testTag("component3"))
+        Text(text = "Ita", Modifier.testTag("component4"))
+        
+        Image(Icons.Default.Add, contentDescription = "sImage")
+        Image(Icons.Default.Add, contentDescription = "xsImage")
+        Image(Icons.Default.Add, contentDescription = "xsImage")
+        Image(Icons.Default.Add, contentDescription = "xsImage")
+    }
 }

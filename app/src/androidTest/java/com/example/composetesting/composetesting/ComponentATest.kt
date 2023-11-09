57c3a7792658bf78ba98bfb67f1d489ad85fa4e7
Ivette C Valdivia Gomez <89611005+ivette2021@users.noreply.github.com>
2023-11-09 18:33:30 -0300

    Actions y assertions

--- app/src/androidTest/java/com/example/composetesting/composetesting/ComponentATest.kt
+++ app/src/androidTest/java/com/example/composetesting/composetesting/ComponentATest.kt
@@ -1,12 +1,37 @@
 package com.example.composetesting.composetesting
 
 
+import androidx.compose.ui.test.assertContentDescriptionContains
+import androidx.compose.ui.test.assertContentDescriptionEquals
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsEnabled
+import androidx.compose.ui.test.assertIsFocused
+import androidx.compose.ui.test.assertIsNotDisplayed
+import androidx.compose.ui.test.assertIsNotEnabled
+import androidx.compose.ui.test.assertIsNotSelected
+import androidx.compose.ui.test.assertIsOff
+import androidx.compose.ui.test.assertIsOn
+import androidx.compose.ui.test.assertIsSelected
+import androidx.compose.ui.test.assertTextContains
+import androidx.compose.ui.test.assertTextEquals
+import androidx.compose.ui.test.doubleClick
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.longClick
 import androidx.compose.ui.test.onAllNodesWithContentDescription
 import androidx.compose.ui.test.onAllNodesWithTag
 import androidx.compose.ui.test.onAllNodesWithText
+import androidx.compose.ui.test.onFirst
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performImeAction
+import androidx.compose.ui.test.performScrollTo
+import androidx.compose.ui.test.performTextInput
+import androidx.compose.ui.test.performTextReplacement
+import androidx.compose.ui.test.performTouchInput
+import androidx.compose.ui.test.swipeDown
+import androidx.compose.ui.test.swipeLeft
+import androidx.compose.ui.test.swipeRight
 import com.example.composetesting.components.ComponentA
 
 import org.junit.Rule
@@ -35,5 +60,42 @@ class ComponentATest {
         //  composeTestRule.onAllNodesWithText(":") //busca todos los textos que tengan dos puntos
         composeTestRule.onAllNodesWithText("i") //busca todos los textos que tengan i puntos
         composeTestRule.onAllNodesWithContentDescription("decoration") //te busca todos los que tengan como descripcion decoracion
+
+        //Actions
+        composeTestRule.onNodeWithText("ivi", ignoreCase = true).performClick()
+        composeTestRule.onAllNodesWithText("i").onFirst().performClick()
+        composeTestRule.onNodeWithText("ivi").performTouchInput {
+            longClick()
+            doubleClick()
+            swipeDown()
+            swipeLeft()
+            swipeRight()
+        }
+        composeTestRule.onNodeWithText("ivi").performScrollTo()
+       // composeTestRule.onNodeWithText("ivi").performScrollTo().performClick().performTextInput("blah blah") //eejmplo de que algunos se pueden continuar en una linea
+        composeTestRule.onNodeWithText("ivi").performImeAction()//se refiere al boton que hace salto de linea o boton azul en el teclado o enter
+        composeTestRule.onNodeWithText("ivi").performTextInput("hola que tal") //este no permite poner otro parametros como . performClick
+        composeTestRule.onNodeWithText("ivi").performTextReplacement("no se que decir")
+
+        //Assertions
+        composeTestRule.onNodeWithText("ivi").assertExists()
+        composeTestRule.onNodeWithText("ivi").assertDoesNotExist()
+        composeTestRule.onNodeWithText("ivi").assertContentDescriptionContains("EADWAD")
+        composeTestRule.onNodeWithText("ivi").assertContentDescriptionEquals("EADWAD")
+        composeTestRule.onNodeWithText("ivi").assertIsDisplayed() //es visible
+        composeTestRule.onNodeWithText("ivi").assertIsNotDisplayed() //no es visible
+        composeTestRule.onNodeWithText("ivi").assertIsEnabled()
+        composeTestRule.onNodeWithText("ivi").assertIsNotEnabled()
+        composeTestRule.onNodeWithText("ivi").assertIsSelected()
+        composeTestRule.onNodeWithText("ivi").assertIsNotSelected()
+        composeTestRule.onNodeWithText("ivi").assertIsFocused()
+        composeTestRule.onNodeWithText("ivi").assertIsOn()
+        composeTestRule.onNodeWithText("ivi").assertIsOff()
+        composeTestRule.onNodeWithText("ivi").assertTextEquals("") //compruebo que el texto es igual a esto ""
+        composeTestRule.onNodeWithText("ivi").assertTextContains("Ivi")
+
+
+
+
     }
 }
--- app/src/main/java/com/example/composetesting/components/ComponentA.kt
+++ app/src/main/java/com/example/composetesting/components/ComponentA.kt
@@ -6,13 +6,21 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.runtime.*
 import androidx.compose.ui.platform.testTag
 
 
 @Composable
 fun ComponentA() {
+    var name by rememberSaveable {
+        mutableStateOf("Ivette")
+    }
+
     Column(Modifier.fillMaxSize()){
         Text(text = "Ivette",Modifier.testTag("component1"))
         Text(text = "Ivi", Modifier.testTag("component2"))
@@ -23,5 +31,8 @@ fun ComponentA() {
         Image(Icons.Default.Add, contentDescription = "xsImage")
         Image(Icons.Default.Add, contentDescription = "xsImage")
         Image(Icons.Default.Add, contentDescription = "xsImage")
+
+        TextField(value = name, onValueChange = {name = it})
+        Text(text = ("Personaje 1: $name"))
     }
 }

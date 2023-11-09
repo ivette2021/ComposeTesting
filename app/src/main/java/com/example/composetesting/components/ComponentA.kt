85f59086b9042287a7829b89980a60da846e1019
Ivette C Valdivia Gomez <89611005+ivette2021@users.noreply.github.com>
2023-11-09 18:33:49 -0300

    Actions y assertions

--- app/src/main/java/com/example/composetesting/components/ComponentA.kt
+++ app/src/main/java/com/example/composetesting/components/ComponentA.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -15,6 +16,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.platform.testTag
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ComponentA() {
     var name by rememberSaveable {
